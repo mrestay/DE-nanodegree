07fe95eee76b969acebbe669143b6bb24311a9ea
Mateo Restrepo <mateorestrepo@factored.ai>
2021-10-28 10:05:29 -0400

    Fix imports

--- 4-airflow_pipelines/plugins/helpers/__init__.py
+++ 4-airflow_pipelines/plugins/helpers/__init__.py
@@ -1,4 +1,4 @@
-from helpers.sql_queries import SqlQueries
+from sql_queries import SqlQueries
 
 __all__ = [
     'SqlQueries',
--- 4-airflow_pipelines/plugins/operators/__init__.py
+++ 4-airflow_pipelines/plugins/operators/__init__.py
@@ -1,7 +1,7 @@
-from operators.stage_redshift import StageToRedshiftOperator
-from operators.load_fact import LoadFactOperator
-from operators.load_dimension import LoadDimensionOperator
-from operators.data_quality import DataQualityOperator
+from stage_redshift import StageToRedshiftOperator
+from load_fact import LoadFactOperator
+from load_dimension import LoadDimensionOperator
+from .data_quality import DataQualityOperator
 
 __all__ = [
     'StageToRedshiftOperator',
